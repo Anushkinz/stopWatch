76d34a601a5fbc5969ba9f86d53b8c941249141a
Anushkinz <anuarprogramm@gmail.com>
2021-05-17 13:10:21 +0600

    Initial Commit

---
+++ stopWatch/stopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  stopWatchApp.swift
+//  stopWatch
+//
+//  Created by admin on 17/5/21.
+//
+
+import SwiftUI
+
+@main
+struct stopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
